87ed3360ad21ecc4c125d835849df7bac88d7c06
p.dirk <paul.dirk@durham.ac.uk>
2025-01-16 16:05:23 +0000

    extracting energy, force, and fermi level
    fermi level also for twochem calculation, as well as some bug fixes

--- read_QE_data.sh
+++ read_QE_data.sh
@@ -4,56 +4,73 @@
 # assume that if ibrav==0 the units in the CELL_PARAMETERS block have been provided in units of Angstrom
 #######################
 natoms=36
-prefix=Ca2FeAlO5.relax
+prefix=Ca2MnAlO5.relax
 outfile=info.dat
 create_poscar=1 #1:true, 0:false
 #######################
 
-input=`echo "$prefix.in"`
-stdout=`echo "$prefix.out"`
+stdin=$(echo "$prefix.in")
+stdout=$(echo "$prefix.out")
 bohr2ang=0.529177
 Ry2eV=13.6057039763 
 
 [ -f "$outfile" ] && rm "$outfile"
 
-energy=$(grep ! $stdout | tail -1 | awk '{print $5}')
+energy=$(grep ! "$stdout" | tail -1 | awk '{print $5}')
 energy_eV=$(echo "$energy * $Ry2eV" | bc -l)
-echo -e "E(eV)= $energy_eV \n" > "$outfile"
+fermi=$(grep "the Fermi energy is" "$stdout" | tail -1 | awk '{print $5}')
+force=$(grep 'Total force' "$stdout" | tail -1 | awk '{print $4}')
+force_eVAng=$(echo "$force * $Ry2eV / $bohr2ang" | bc -l)
+
+echo -e "E(eV)= $energy_eV" > "$outfile"
+echo -e "E_f(eV)= $fermi" >> "$outfile"
+# if using twochem calculation
+twochem_tag=$(grep 'twochem' "$stdin" | tr "." "\n" | awk 'NR==2')
+if [[ $twochem_tag == "true" ]]; then
+  fermi_cond=$(grep "the conduction Fermi energy is" "$stdout" | tail -1 | awk '{print $6}')
+  echo -e "cond. E_f(eV)= $fermi_cond" >> "$outfile"
+fi
+
+echo -e "F_tot(eV/Ang)= $force_eVAng" >>  "$outfile"
+
 
 # Read Cell parameters block
-matrix=$(grep -A 3 CELL_PARAMETERS $stdout | tail -3)
+IFS='$\n'
+matrix=$(grep -A 3 CELL_PARAMETERS "$stdout" | tail -3)
+unset IFS
 
 # Read lattice type from the input file
-ibrav=$(grep 'ibrav' Ca2FeAlO5.relax.in | awk '{print $3}' | rev | cut -c2- | rev)
+ibrav=$(grep 'ibrav' "$prefix.in" | awk '{print $3}' | rev | cut -c2- | rev)
 
-
-if [ $ibrav -ne 0 ]; then
+if [ "$ibrav" -ne 0 ]; then
   # Convert lattice matrix from Bohr to Angstrom
-  alat=$(grep CELL_PARAMETERS $stdout -A 3 | head -1 | awk '{print $3}'| rev | cut -c2- | rev)
+  alat=$(grep CELL_PARAMETERS "$stdout" -A 3 | head -1 | awk '{print $3}'| rev | cut -c2- | rev)
   result=""
   while IFS= read -r row; do
     row_result=""
     for value in $row; do
       transformed_value=$(echo "$value * $alat * $bohr2ang" | bc -l)
-      row_result="$row_result$(printf "%.8f " $transformed_value)"
+      row_result="$row_result$(printf "%.8f " "$transformed_value")"
     done
     result="$result$row_result\n"
   done <<< "$matrix"
   # Add cell parameter block (in Angstrom) to info file
   IFS='$\n'
-  celldm=`grep 'celldm(1)' $stdout -A 2 | tail -3`
+  celldm=$(grep 'celldm(1)' "$stdout" -A 2 | tail -3)
   echo -e "$celldm \n" >> "$outfile"
   echo "CELL_PARAMETERS (angstrom)" >> "$outfile"
 else
   # Cell parameters are already in Angstrom
+  IFS='$\n'
   result=$matrix
   echo "CELL_PARAMETERS (angstrom)" >> "$outfile"
 fi
   echo -e "$result" >> "$outfile"
 
 # Add positions to info file
-positions=$(grep ATOMIC_POSITIONS Ca2FeAlO5.relax.out -A $natoms | tail -$(($natoms + 1)))
-echo -e $positions >> "$outfile"
+IFS='$\n'
+positions=$(grep ATOMIC_POSITIONS "$prefix.out" -A $natoms | tail -$(($natoms + 1)))
+echo -e "$positions" >> "$outfile"
 
 # Check whether POSCAR file creation is requested
 if [ $create_poscar -eq 0 ]; then
@@ -75,7 +92,7 @@ poscar="# Converted POSCAR from QE output"
 poscar="$poscar\n1.0"
 
 # Add lattice matrix output to POSCAR
-poscar=$(echo "$poscar\n$result" | tr -d '\n')
+poscar=$(echo -e "$poscar\n$result")
 
 # Extract element names and counts (remove any numbers)
 mapfile -t elements < <(echo "$atomic_positions" | awk '{print $1}' | tr -d '0123456789' | uniq)
@@ -88,7 +105,7 @@ for element in "${elements[@]}"; do
 done
 
 # Add the element names and counts to the POSCAR
-poscar="$poscar$(IFS=" "; echo "${elements[*]}")"  # Atom types in one line
+poscar="$poscar\n$(IFS=" "; echo "${elements[*]}")"  # Atom types in one line
 poscar="$poscar\n$(printf "%d " "${counts[@]}")"    # Counts
 
 # Add the coordinates type (Direct)
